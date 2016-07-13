a03a8c1e4dd67357dfc1fdbb4456819d6d1c0e9a
Herzog Langbart <che@revolution.localdomain>
2016-07-13 02:47:42 -0700

    Use x*alloc when creating a new matrix.
    	Time to use the new common functions.  Note that xcalloc is
    	only called when allocating each column; there is no need to
    	clear the memory allocated to the struct or row array because
    	we will be assigning values there within this function before
    	we return the matrix.
    
    	* lib/mtx_new.c: Use _xmalloc and _xcalloc.

--- lib/mtx_new.c
+++ lib/mtx_new.c
@@ -1,19 +1,25 @@
 /* Make a new matrix ROWS rows by COLUMNS columns, elements
    initialized to zero.  */
 
+#include "common.h"
 #include "matrix.h"
 
 struct matrix *
 mtx_new (size_t rows, size_t columns)
 {
-	struct matrix *m = (struct matrix *) calloc (1, sizeof (struct matrix));
+	size_t i;
+	struct matrix *m;
+
+	m = (struct matrix *) _xmalloc (sizeof (struct matrix));
 
 	m->rows = rows;
 	m->cols = columns;
-	m->data = (double **) calloc (rows, sizeof (double *));
-	size_t i;
-	for (i = 0;  i < rows; i++ )
-		m->data[i] = (double *) calloc (columns, sizeof (double));
+
+	// C is row-major
+	m->data = (double **) _xmalloc (rows * sizeof (double *));
+
+	for (i = 0; i < rows; ++i)
+		m->data[i] = (double *) _xcalloc (columns, sizeof (double));
 
 	return m;
 }
