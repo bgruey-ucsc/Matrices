f26f2df8f2e0c4fba804e6b5af368c98215f40cb
Herzog Langbart <che@revolution.localdomain>
2016-07-13 02:54:31 -0700

    Fix up mtx_delete.
    	Not a seemingly substantial change, but important.  The
    	previous comment indicated a dangling pointer to invalid
    	memory was leftover---that is bad, so we set the pointer to
    	NULL ourselves, before returning.
    
    	Some coding style cleanup was also performed, separating
    	variable declarations from functional code and making the code
    	block nice and tidy.
    
    	* lib/mtx_delete.c: Set the pointer to the free'd struct
    	matrix to NULL after free'ing, and organize code.

--- lib/mtx_delete.c
+++ lib/mtx_delete.c
@@ -1,15 +1,16 @@
-/* Delete all the data of a matrix.  Does not delete the pointer
-   though, that's your job.  */
+/* Delete a matrix, freeing the memory originally allocated to it.  */
 
 #include "matrix.h"
 
 void
 mtx_delete (struct matrix *m)
 {
-	size_t i = 0;
-	for (i = 0; i < m->rows; i++)
-		free (m->data[i]); // arrays of column entries
+	size_t i;
 
+	// arrays of column entries
+	for (i = 0; i < m->rows; i++)
+		free (m->data[i]);
 	free (m->data); // array of rows
 	free (m); // rows, cols and data
+	m = NULL;
 }
